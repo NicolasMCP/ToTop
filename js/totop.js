138261651559fc4806c0485091a8cf9398042aa6
NicolasMCP <nicolas.mcp@gmail.com>
2018-06-09 09:09:48 -0300

    progeto de testes do botão totop

---
+++ js/totop.js
@@ -0,0 +1,35 @@
+(function($) {
+    $.fn.totop = function () {
+        let to_btn = this;
+        $(document).scroll(function () {
+            let pos = $(window).scrollTop();
+
+            if (pos >= 70) {
+                if (! (to_btn.hasClass('primeira'))) {
+                    to_btn.addClass('primeira')
+                        .css({'right': 40,
+                              'bottom': 40,
+                              'position': 'fixed',
+                              'display': 'none',
+                        })
+                        .fadeIn(500);
+                } else {
+                    to_btn.fadeIn(500);
+                }
+            } else {
+                to_btn.fadeOut(500);
+            }
+        });
+        to_btn.hover(function () {
+            to_btn.css('opacity','1');
+        }, function () {
+            to_btn.css('opacity','.7');
+        }).click(function () {
+            $("html, body").animate({
+                scrollTop: 0
+            }, {
+                duration: 900
+            });
+        });
+    }
+}(jQuery));
